184ef4a878ea3bc3b52a861b5c245b188f61a125
Jith Kumar <jith.kumar@swift-nav.com>
2023-03-23 17:39:17 +1100

    Add support for maps and nmea

--- pimenu.py
+++ pimenu.py
@@ -4,10 +4,22 @@ from tkinter import constants as TkC
 import os
 import subprocess
 import sys
-from tkinter import Tk, Frame, Button, Label, PhotoImage, Scrollbar, Text
+from tkinter import Tk, Frame, Button, Label, PhotoImage, Scrollbar, Text, LabelFrame, StringVar
 from math import sqrt, floor, ceil
 import threading
 import yaml
+import time
+
+import pynmea2.pynmea2 as pynmea2
+
+# need pip3 install tkintermapview --upgrade
+import tkintermapview
+
+kScreenWidth=800
+kScreenHeight=480
+kMapRefreshRate=2 # in seconds
+
+special_actions = ['showMap']
 
 class IconCache():
     icons = {}
@@ -82,28 +94,78 @@ class Runner():
     icons = {}
     path = ''
     iconCache = IconCache()
-    def __init__(self, command, frame):
+    process = None
+    old_sys_out = None
+    def __init__(self, command, frame, bg=None, map_widget=None):
         self.command = command
         self.frame = frame
         self.main_thread = threading.Thread(target=self.start)
         self.path = os.path.dirname(os.path.realpath(sys.argv[0]))
+        self.bg=bg
+        self.map_widget=map_widget
+        self.first_marker=None
+        self.last_marker=None
+        self.title_var=None
     def run(self):
         self.main_thread.start()
     def start(self):
         print("Thread: start")
-        p = subprocess.Popen(self.command, stdout=subprocess.PIPE, bufsize=1, text=True)
-        while p.poll() is None:
-            msg = p.stdout.readline().strip() # read a line from the process output
-            if msg:
+        self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        # i = 0
+        last_time = time.time()
+        last_mode = 0
+        while self.process.poll() is None:
+            msg = self.process.stdout.readline().strip() # read a line from the process output
+            if msg and 'GPGGA' not in msg:
                 print(msg)
+            elif 'GPGGA' in msg and self.map_widget is not None:
+                nmea = pynmea2.parse(msg)
+                if not nmea.is_valid:
+                    continue
+                gps_qual = nmea.gps_qual
+                if time.time() - last_time < kMapRefreshRate and gps_qual == last_mode:
+                    continue
+                last_time = time.time()
+                last_mode = gps_qual
+                color_circle='#caf0f8'
+                color_outside='#ff006e'
+                title=None
+                colors_map = {
+                    1:('#ff006e', 'SPP'), # SPP
+                    2:('#fb5607', 'DGPS'),
+                    4:('#06d6a0', 'RTK Fixed'),
+                    5:('#3a86ff', 'RTK Float'),
+                    6:('#6d6875', 'DR'),
+                }
+                if gps_qual in colors_map.keys():
+                    color_outside,title = colors_map[gps_qual]
+                marker = self.map_widget.set_marker(nmea.latitude, nmea.longitude, marker_color_circle=color_circle, marker_color_outside=color_outside)
+                self.map_widget.set_position(nmea.latitude, nmea.longitude)
+                if self.title_var is not None and title is not None:
+                    self.title_var.set(title)
+                if self.first_marker is None:
+                    self.first_marker = marker
+                elif self.last_marker is None:
+                    self.last_marker = marker
+                if self.last_marker is not None:
+                    self.last_marker.delete()
+                    self.last_marker = marker
+                # print("Done {}".format(i))
+                # i += 1
         print("Thread: end")
+        self.process = None
     def stop(self):
-        subprocess.call(['killall', '-9', self.command[0]])
+        sys.stdout = self.old_sys_out
+        if self.process is not None:
+            self.process.kill()
+        # self.join()
 
-
-    def trigger(self, command=None):
-        text = Text(self.frame)
-        text.pack(side='left', fill='both', expand=True)
+    def trigger(self, map_widget=None, title_var=None):
+        if map_widget is None:
+            text = Text(self.frame)
+        else:
+            text = Text(self.frame, height=4, bg=self.bg)
+        text.pack(side='left', fill='x', expand=True)
 
         scrollbar = Scrollbar(self.frame)
         scrollbar.pack(side='right', fill='y')
@@ -111,16 +173,11 @@ class Runner():
         text['yscrollcommand'] = scrollbar.set
         scrollbar['command'] = text.yview
 
-        old_stdout = sys.stdout
+        self.old_sys_out = sys.stdout
         sys.stdout = Redirect(text)
-
-        button = FlatButton(self.frame, text='TEST', command=self.run, image=self.iconCache.get_icon("monitor"),)
-        button.pack()
-        button2 = FlatButton(self.frame, text='Stop', command=self.stop, image=self.iconCache.get_icon("sign-error"),)
-        button2.pack()
-        if command is not None:
-          closeButton = FlatButton(self.frame, text='Close', command=command, image=self.iconCache.get_icon("sign-left"),)
-          closeButton.pack()
+        self.map_widget = map_widget
+        self.title_var = title_var
+        self.run()
 
     def join(self):
         self.main_thread.join()
@@ -233,6 +290,8 @@ class PiMenu(Frame):
                 btn.configure(command=lambda act=act, item=item: self.show_items(item['items'], act),
                               text=item['label'] + '…')
                 btn.set_color("#2b5797")  # dark-blue
+            elif item['name'] in special_actions:
+                btn.configure(command=lambda item=item: self.do_special_action(item), )
             else:
                 # this is an action
                 btn.configure(command=lambda item=item: self.go_action(item), )
@@ -280,6 +339,42 @@ class PiMenu(Frame):
         while len(self.framestack) > 1:
             self.destroy_top()
 
+    def do_special_action(self, item):
+        # hide the menu and show a delay screen
+        self.hide_top()
+        command = item['name']
+        label = item['label']
+        color = "#2b5797"
+        if 'command' in item:
+            command = item['command']
+        actionWindow = Frame(self, bg=color)
+        actionWindow.pack(fill=TkC.BOTH, expand=1)
+        labelFrame = LabelFrame(actionWindow, borderwidth=0, border=0, height=1, bg=color)
+        p1 = Runner(command.split(), labelFrame, bg=color)
+        def destroy(self, actionWindow):
+            p1.stop()
+            # remove actionWindow screen and show menu again
+            actionWindow.destroy()
+            self.destroy_all()
+            self.show_top()
+        
+        title_var = StringVar(value=label)
+        label = Label(labelFrame, height=1, textvariable=title_var, fg="white", bg=color, font="Sans 30")
+        label.pack(side='left')
+
+        backButton = FlatButton(labelFrame, text='Close', command=lambda  self=self, actionWindow=actionWindow: destroy(self, actionWindow),
+                                 image=self.iconCache.get_icon("sign-left"))
+        backButton.pack(side='right')
+        labelFrame.pack(side='top', fill=TkC.X, expand=1)
+        map_widget = tkintermapview.TkinterMapView(actionWindow, corner_radius=0, width=kScreenWidth, height=int(kScreenHeight*.8))
+        map_widget.pack(side='left',fill=TkC.BOTH, expand=1)
+        map_widget.set_zoom(16)
+        self.parent.update()
+
+        p1.trigger(map_widget=map_widget, title_var=title_var)
+
+        return
+
     def go_action(self, item):
         """
         execute the action script
@@ -303,12 +398,17 @@ class PiMenu(Frame):
 
         def destroy(self, actionWindow):
             # remove actionWindow screen and show menu again
+            p1.stop()
             actionWindow.destroy()
             self.destroy_all()
             self.show_top()
 
-        p1 = Runner(command.split(), actionWindow)
-        p1.trigger(command=lambda  self=self, actionWindow=actionWindow: destroy(self, actionWindow))
+        p1 = Runner(command.split(), actionWindow, bg=color)
+
+        backButton = FlatButton(actionWindow, text='Close', command=lambda  self=self, actionWindow=actionWindow: destroy(self, actionWindow),
+                                 image=self.iconCache.get_icon("sign-left"))
+        backButton.pack(side='right')
+        p1.trigger()
 
     def go_back(self):
         """
@@ -325,7 +425,7 @@ class PiMenu(Frame):
 
 def main():
     root = Tk()
-    root.geometry("{}x{}".format(800,480))
+    root.geometry("{}x{}".format(kScreenWidth,kScreenHeight))
     root.wm_title('PiMenu')
     if len(sys.argv) > 1 and sys.argv[1] == 'fs':
         root.wm_attributes('-fullscreen', True)
